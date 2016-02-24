3d199427ce3eae4e516172b720c5308b7e7d80bf
Jonathan Davila <jdavila@redhat.com>
2016-02-23 21:12:57 -0600

    save

--- SA.py
+++ SA.py
@@ -74,6 +74,7 @@ def get_all_nics(vm_obj):
         if isinstance(device, vim.vm.device.VirtualEthernetCard):
             nicspec = vim.vm.device.VirtualDeviceSpec()
             print dir(device)
+            print device.deviceInfo
 
 
 def get_vm_object(module, conn, path, datacenter):
