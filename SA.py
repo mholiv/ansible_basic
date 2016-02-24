23b0278c950f08b27cb1d84815e2c5d729d138fd
Jonathan Davila <jdavila@redhat.com>
2016-02-24 09:52:54 -0600

    save

--- SA.py
+++ SA.py
@@ -78,7 +78,7 @@ def get_all_nics(vm_obj):
             print device.backing
             print device.slotInfo
             print device.__class__.__name__
-            print device._type
+            print type(device)
 
 
 def objwalk(obj, path_elements):
