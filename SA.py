7285e3b4aaea117b2f2593695e78bc7730485c59
Jonathan Davila <jdavila@redhat.com>
2016-02-23 19:34:23 -0600

    save

--- SA.py
+++ SA.py
@@ -84,10 +84,10 @@ def get_vm_object(module, conn, path, datacenter):
 
     try:
         if len(matching_vms) > 1:
-            path_elements = []
+            elements = []
             for vm_obj in matching_vms:
                 print 'path is:', path
-                print objwalk(vm_obj, path_elements)
+                print objwalk(vm_obj, elements)
                 # if path_matches(vm_obj, path_list):
                 #     return vm_obj
         else:
