9f1e9c60b3fafcb120609a0defe7baf486ee8ea5
Jonathan Davila <jdavila@redhat.com>
2016-02-25 07:08:35 -0600

    save

--- SA.py
+++ SA.py
@@ -227,11 +227,11 @@ def main():
     conn = connect_to_api()
     proper_vm = get_vm_object(module, conn, path, datacenter)
     all_nics = get_nics(proper_vm)
-
-    if state == 'create':
-        print create_nic(module, conn, proper_vm, desired_nic)
-    elif state == 'absent':
-        print remove_nic(module, conn, proper_vm, desired_nic, all_nics)
+    print all_nics
+    # if state == 'create':
+    #     print create_nic(module, conn, proper_vm, desired_nic)
+    # elif state == 'absent':
+    #     print remove_nic(module, conn, proper_vm, desired_nic, all_nics)
         
 
     else:
