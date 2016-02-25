e9ab66b762c999d9c200d5a6712b148dc5274f25
Jonathan Davila <jdavila@redhat.com>
2016-02-25 06:31:08 -0600

    save

--- SA.py
+++ SA.py
@@ -177,8 +177,8 @@ def main():
     all_nics = get_nics(proper_vm)
 
     if state == 'present':
-        #print all_nics
-        print create_nic(module, conn, proper_vm, desired_nic)
+        print all_nics
+        #print create_nic(module, conn, proper_vm, desired_nic)
         
 
     else:
