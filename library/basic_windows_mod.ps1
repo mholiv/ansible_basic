cbc050416fcee8a6842b0a09b1355492659e08ac
Caitlin Sovis <caitlin@dell-76789.local>
2016-07-13 20:14:50 -0400

    added gpl

--- library/basic_windows_mod.ps1
+++ library/basic_windows_mod.ps1
@@ -1,6 +1,21 @@
 #!powershell
 # The above line tells us that this is a powershell module
 
+<#
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
 #These lines tell us that we want to import the Ansible powershell common library
 #THis is analogous to the "this is magic" in python.
 # WANT_JSON
